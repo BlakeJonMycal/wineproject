c203f0973415c69d3a926db79fe980e79c2514b5
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-07-18 15:58:32 -0400

    changed naming convention on wine styles model

--- wineapi/models/wine_style.py
+++ wineapi/models/wine_style.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class WineStyle(models.Model):
     wine = models.ForeignKey('Wine', related_name='wine_styles', on_delete=models.CASCADE)
-    categorization = models.ForeignKey('Style', related_name='category_wines', on_delete=models.CASCADE)
+    style = models.ForeignKey('Style', related_name='category_wines', on_delete=models.CASCADE)
