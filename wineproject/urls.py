5f235fc84ce44df76b1c77954a5eb837db8a66d2
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-07-19 14:39:47 -0400

    database is seeded and styles view has been updated

--- wineproject/urls.py
+++ wineproject/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from wineapi.views import register_user, login_user
+from wineapi.views import register_user, login_user, StyleViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'styles', StyleViewSet, 'style')
+
 
 urlpatterns = [
     path('', include(router.urls)),
