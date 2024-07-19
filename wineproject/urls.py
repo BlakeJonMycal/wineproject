452287961f7f19c4fcd870c09f5511246ea4cd53
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-07-19 15:42:55 -0400

    list all wines

--- wineproject/urls.py
+++ wineproject/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from wineapi.views import register_user, login_user, StyleViewSet
+from rest_framework.routers import DefaultRouter
+from wineapi.views import UserViewSet, StyleViewSet, WineViewSet
 
-router = routers.DefaultRouter(trailing_slash=False)
+router = DefaultRouter(trailing_slash=False)
 router.register(r'styles', StyleViewSet, 'style')
+router.register(r'wines', WineViewSet, 'wine')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('register', register_user), 
-    path('login', login_user), 
+    path('login', UserViewSet.as_view({'post': 'user_login'}), name='login'),
+    path('register', UserViewSet.as_view({'post': 'register_account'}), name='register'),
 ]
 
